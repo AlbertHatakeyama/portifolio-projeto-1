b5593d32d95ae271bec223435754609090d8217b
AlbertHatakeyama <ahnabarrete@gmail.com>
2024-03-08 17:28:58 -0300

    seguranças

--- consultoria-seguranca-cibernetica.php
+++ consultoria-seguranca-cibernetica.php
@@ -44,19 +44,69 @@ include("header.php");
         </div>
     </div>
 </section>
-<section class="bg-dark">
+<section class="section-cirber">
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-6 col-12 ">
                 <img src="assets/images/apresentacao/ciber-seguranca.jpg" alt="" class="img-fluid borda-r">
             </div>
-            <div class="col-md-6 col-12 pt-5">
+            <div class="col-md-6 col-12 pt-5 text-white">
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    <br>
+                    <br>
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    <br>
+                    <br>
+                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages
+                    <br>
+                    <br>
+                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+                </p>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-12">
                 <h3 class="fw-bold fs-4 text-white">Nossa Solução de Segurança Cibernética</h3>
-                <div class="features-item text-white my-2">
-                    <div class="icon">
-                        <i class="fa-solid fa-microchip"></i>
-                        <h2>Lorem ipsum</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                <div class="col-md-3 col-6">
+                    <div class="features-item text-white my-2">
+                        <div class="icon">
+                            <i class="fa-solid fa-microchip"></i>
+                            <h2>Lorem ipsum</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="features-item text-white my-2">
+                        <div class="icon">
+                            <i class="fa-solid fa-microchip"></i>
+                            <h2>Lorem ipsum</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="features-item text-white my-2">
+                        <div class="icon">
+                            <i class="fa-solid fa-microchip"></i>
+                            <h2>Lorem ipsum</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="features-item text-white my-2">
+                        <div class="icon">
+                            <i class="fa-solid fa-microchip"></i>
+                            <h2>Lorem ipsum</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        </div>
                     </div>
                 </div>
             </div>
